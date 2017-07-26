7e6d8759a94c372b95e237eaff1c076bd99834cb
vikas <vikasrajuinversity@gmail.com>
2017-07-26 05:45:44 -0700

    add files"

--- Django_Project2/Demoapp2/settings.py
+++ Django_Project2/Demoapp2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Demoapp2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Demoapp2.urls'
+ROOT_URLCONF = 'Django_Project2.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Demoapp2.wsgi.application'
+WSGI_APPLICATION = 'Django_Project2.wsgi.application'
 
 
 # Database
