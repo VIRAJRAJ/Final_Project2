d4f70b7d1f1f615896e84a4c66cc00338fd8a10a
vikas <vikasrajuinversity@gmail.com>
2017-07-26 08:44:41 -0700

    add all scatterd files

--- Django_Pproject/Django_Pproject/urls.py
+++ Django_Pproject/Django_Pproject/urls.py
@@ -1,4 +1,4 @@
-"""Demoapp2 URL Configuration
+"""Django_Pproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
--- Django_Pproject/Django_Pproject/wsgi.py
+++ Django_Pproject/Django_Pproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Demoapp2 project.
+WSGI config for Django_Pproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Project2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Pproject.settings")
 
 application = get_wsgi_application()
